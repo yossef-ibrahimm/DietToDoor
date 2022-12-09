25f783f1c8ca6c24420f210b1218b17ae92288a8
yossef-ibrahimm <96955894+yossef-ibrahimm@users.noreply.github.com>
2022-12-09 13:26:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let toggle__btn = document.querySelector(".toggle__btn");
+let resbonsive_navbar = document.querySelector(".resbonsive_navbar");
+toggle__btn.onclick = function () {
+  resbonsive_navbar.classList.toggle("block");
+};
