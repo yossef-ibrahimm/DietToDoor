b4e09877f058c8b9f726bf20fb927b0d29f4237a
yossef-ibrahimm <96955894+yossef-ibrahimm@users.noreply.github.com>
2023-05-28 19:58:15 +0300

    some clean for code
    some clean for code

--- js/index.js
+++ js/index.js
@@ -1,109 +1,99 @@
 function replace_string(e) {
-  e.value = e.value.replace(/[^0-9.]/g, "");
+  e.value = e.value.replace(/[^0-9.]/g, '')
 }
 /* follow up function to open and close page it block on click and add animation  */
 function followUpPage(popId, openButton, closeBtn) {
-  const openFormBtn = document.querySelectorAll(openButton);
-  const closeFormBtn = document.getElementById(closeBtn);
-/*   const Mainpop = document.querySelectorAll(".pop");
- */  const pop = document.getElementById(popId);
-  const form = document.querySelector(`#${popId} #myForm`);
-  const html = document.querySelector("html");
+  const openFormBtn = document.querySelectorAll(openButton)
+  const closeFormBtn = document.getElementById(closeBtn)
+  const pop = document.getElementById(popId)
+  const form = document.querySelector(`#${popId} #myForm`)
+  const html = document.querySelector('html')
   openFormBtn.forEach((e) => {
-    e.addEventListener("click", () => {
-      document.documentElement.scrollTop = 0;
-      pop.style.display = "grid";
-      html.style.cssText = "overflow:hidden;";
+    e.addEventListener('click', () => {
+      document.documentElement.scrollTop = 0
+      pop.style.display = 'grid'
+      html.style.cssText = 'overflow:hidden;'
       form.animate(
         [
-          { transform: "scale(0.5)", opacity: 0 },
-          { transform: "scale(1)", opacity: 1 },
+          { transform: 'scale(0.5)', opacity: 0 },
+          { transform: 'scale(1)', opacity: 1 },
         ],
         {
           duration: 500,
-          easing: "ease-out",
+          easing: 'ease-out',
         }
-      );
-    });
-  });
+      )
+    })
+  })
   function close() {
     form.animate(
       [
-        { transform: "scale(1)", opacity: 1 },
-        { transform: "scale(0.5)", opacity: 0 },
+        { transform: 'scale(1)', opacity: 1 },
+        { transform: 'scale(0.5)', opacity: 0 },
       ],
       {
         duration: 500,
-        easing: "ease-out",
+        easing: 'ease-out',
       }
     ).onfinish = () => (
-      (pop.style.display = "none"), (html.style.cssText = " overflow-x:hidden;")
-    );
+      (pop.style.display = 'none'), (html.style.cssText = ' overflow-x:hidden;')
+    )
   }
-  closeFormBtn.addEventListener("click", () => {
-    close();
-  });/*
-  Mainpop.forEach((e) => {
-    e.addEventListener("click", () => {
-    close();
-  });
-  }) */
-
+  closeFormBtn.addEventListener('click', () => {
+    close()
+  })
   /* function to get the data from upload button on follow up page and add some style to input */
   function uploadInputFollowUpPage(input, fileName, fileSize, label) {
-    const inputFile = document.querySelector(`.${input}`);
-    const fileNameField = document.querySelector(`.${fileName}`);
-    const fileSizeField = document.querySelector(`.${fileSize}`);
-    const fileIcon = document.querySelector(`${label} i`);
-    inputFile.addEventListener("change", function () {
-      fileIcon.style.color = "#A0B63B";
-      const uploadedFileName = inputFile.files[0].name;
-      fileNameField.textContent = uploadedFileName;
+    const inputFile = document.querySelector(`.${input}`)
+    const fileNameField = document.querySelector(`.${fileName}`)
+    const fileSizeField = document.querySelector(`.${fileSize}`)
+    const fileIcon = document.querySelector(`${label} i`)
+    inputFile.addEventListener('change', function () {
+      fileIcon.style.color = '#A0B63B'
+      const uploadedFileName = inputFile.files[0].name
+      fileNameField.textContent = uploadedFileName
       const uploadedFileSize =
-        Math.floor(inputFile.files[0].size / 1024) +
-        "kb" +
-        " . " +
-        getDateNow();
-      fileSizeField.textContent = uploadedFileSize;
-    });
+        Math.floor(inputFile.files[0].size / 1024) + 'kb' + ' . ' + getDateNow()
+      fileSizeField.textContent = uploadedFileSize
+    })
   }
   uploadInputFollowUpPage(
-    "follow_upload",
-    "file-name-follow",
-    "file-size-follow",
-    ".upload-label2"
-  );
+    'follow_upload',
+    'file-name-follow',
+    'file-size-follow',
+    '.upload-label2'
+  )
   uploadInputFollowUpPage(
-    "contact_upload",
-    "file-name-contact",
-    "file-size-contact",
-    ".upload-label1"
-  );
+    'contact_upload',
+    'file-name-contact',
+    'file-size-contact',
+    '.upload-label1'
+  )
 }
-followUpPage("pop-follow", ".follow", "closeForm");
-followUpPage("pop-ContactUs", "#contact", "closeFormContact");
+followUpPage('pop-follow', '.follow', 'closeForm')
+followUpPage('pop-ContactUs', '#contact', 'closeFormContact')
 /* function return date now by formate dd / mm/ yy */
 function getDateNow() {
-  const today = new Date();
-  const yyyy = today.getFullYear();
-  let mm = today.getMonth() + 1; // Months start at 0!
-  let dd = today.getDate();
-  if (dd < 10) dd = "0" + dd;
-  if (mm < 10) mm = "0" + mm;
-  const formattedToday = dd + "/" + mm + "/" + yyyy;
-  return formattedToday;
+  const today = new Date()
+  const yyyy = today.getFullYear()
+  let mm = today.getMonth() + 1 // Months start at 0!
+  let dd = today.getDate()
+  if (dd < 10) dd = '0' + dd
+  if (mm < 10) mm = '0' + mm
+  const formattedToday = dd + '/' + mm + '/' + yyyy
+  return formattedToday
 }
 /*  function open and close responsive nav bar */
 function responsiveHamburgerButton() {
-  const hamburgerButton = document.querySelector(".hamburger");
-  const responsiveNavbar = document.querySelector(".responsive-navbar");
+  const hamburgerButton = document.querySelector('.hamburger')
+  const responsiveNavbar = document.querySelector('.responsive-navbar')
 
-  hamburgerButton.addEventListener("click", () => {
-    hamburgerButton.classList.toggle("active");
-    responsiveNavbar.classList.toggle("flex");
+  hamburgerButton.addEventListener('click', () => {
+    hamburgerButton.classList.toggle('active')
+    responsiveNavbar.classList.toggle('flex')
     setTimeout(() => {
-      responsiveNavbar.classList.toggle("active");
-    }, 100);
-  });
+      responsiveNavbar.classList.toggle('active')
+    }, 200)
+  })
 }
-responsiveHamburgerButton();
+responsiveHamburgerButton()
